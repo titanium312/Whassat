606de8956eab525d32d34c5f58ad1e4af5783a48
roberto <rb.roberto.barreto@gmail.com>
2026-03-12 17:25:04 -0500

    deploy

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ status: "API running on Vercel" });
+  res.json({ status: "API running" });
 });
 
 export default app;
